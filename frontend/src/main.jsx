4707433c0115f3a1bf3e03281b56e2b147b5135c
cergibwale-cpu <cergibwale@gmail.com>
2026-02-19 07:03:05 -0600

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
-import Admin from './Admin'; 
+import Admin from './pages/Admin'; 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
